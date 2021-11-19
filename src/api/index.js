7a9c65fc1cb0050ed4723f2bc926abfdcf1ab710
김승현 <ksh7669@naver.com>
2021-11-19 02:03:26 +0000

    Update src/api/index.js

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { API_BASE_URL } from "@/config";
+import { API_BASE_URL, APT_SHOP_URL } from "@/config/index.js";
 
 function apiInstance() {
   const instance = axios.create({
@@ -11,4 +11,14 @@ function apiInstance() {
   return instance;
 }
 
-export { apiInstance };
+function shopInstance() {
+  const instance = axios.create({
+    baseURL: APT_SHOP_URL,
+    headers: {
+      "Content-type": "application/json",
+    },
+  });
+  return instance;
+}
+
+export { apiInstance, shopInstance };
