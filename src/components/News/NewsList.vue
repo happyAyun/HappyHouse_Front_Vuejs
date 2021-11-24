13bcf3556fd869abc945968dd67bf7eee45942ca
Ayun Sim <shim52111@gmail.com>
2021-11-25 03:52:27 +0900

    css

--- src/components/News/NewsList.vue
+++ src/components/News/NewsList.vue
@@ -3,61 +3,30 @@
     <b-container class="bv-example-row mt-3">
       <b-row>
         <b-col>
-          <b-alert show variant="warning"><h1>News</h1></b-alert>
+          <b-alert show variant="danger"
+            ><h1>어제와 오늘의 기사 이슈</h1></b-alert
+          >
         </b-col>
       </b-row>
       <div>
         <b-table striped hover :items="items"></b-table>
       </div>
-      <!-- <vue-good-table
-        :columns="columns"
-        :rows="rows"
-        @on-row-click="viewArticle"
-        :search-options="{
-          enabled: true,
-          placeholder: '검색어를 입력하세요',
-        }"
-        :pagination-options="{
-          enabled: true,
-          mode: 'pages',
-          perPage: 5,
-        }"
-      /> -->
     </b-container>
   </div>
 </template>
 
 <script>
 import { crawling } from "@/api/news";
-//import { mapState, mapActions } from "vuex";
-// import "vue-good-table/dist/vue-good-table.css";
-// import { VueGoodTable } from "vue-good-table";
-
-//const newsStore = "newsStore";
-
 export default {
   name: "NewsList",
-  // components: {
-  //   VueGoodTable,
-  // },
+
   data() {
     return {
       items: [],
-      fields: ["title"],
-      // columns: [
-      //   {
-      //     label: "제목",
-      //     field: "title",
-      //   },
-      // ],
-      // rows: [],
     };
   },
-  computed: {
-    //...mapState(newsStore, ["news"]),
-  },
+  computed: {},
   created() {
-    // this.start();
     crawling(
       (response) => {
         console.log(response.data.titles);
@@ -72,7 +41,7 @@ export default {
           // console.log(item);
           // list.push(item);
           let item = {
-            title: titles[i],
+            "어제와 오늘의 기사 이슈": titles[i],
           };
           console.log(typeof item);
           list.push(item);
@@ -88,18 +57,6 @@ export default {
     );
   },
   methods: {
-    // ...mapActions(newsStore, ["startCraw"]),
-    // start() {
-    //   this.startCraw(
-    //     console.log("시작"),
-    //     (response) => {
-    //       console.log(response);
-    //       this.rows = response.data;
-    //     },
-    //     (error) => {
-    //       console.log(error);
-    //     }
-    //   );
     viewArticle(row) {
       window.location.replace(row.row.url);
     },
--- src/views/WebCraw/News.vue
+++ src/views/WebCraw/News.vue
@@ -1,12 +1,26 @@
 <template>
   <div>
-    <router-view />
+    <b-container class="mt-4">
+      <div class="logo">
+        HappyHouse
+      </div>
+      <PritiNavbar />
+      <b-container class="bv-example-row mt-3 text-center">
+        <div class="top">
+          <router-view />
+        </div>
+      </b-container>
+    </b-container>
   </div>
 </template>
 
 <script>
+import PritiNavbar from "@/components/PritiNavbar.vue";
 export default {
   name: "News",
+  components: {
+    PritiNavbar,
+  },
 };
 </script>
 
